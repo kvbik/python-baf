966aadda3a748797c15509622fd3ccc48935a7e1
Jakub Vysoky <jakub@borka.cz>
2012-10-12 15:20:14 +0200

    all important distribution values

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from distutils.core import setup
 setup(
     name='baf',
     version='1.0',
+    description='example packaging layout',
+    license='BSD',
+    author='Jakub Vysoky',
+    author_email='jakub.vysoky@gmail.com',
+    url='https://github.com/kvbik/python-baf',
     packages=['baf'],
 )
 
